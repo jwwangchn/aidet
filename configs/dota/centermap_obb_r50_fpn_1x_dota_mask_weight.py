c7366a3259e2a28279e409dd00753f5fad734774
jwwangchn <jwwangchn@outlook.com>
2020-05-11 20:01:56 +0800

    add mask weight to dota dataset

--- configs/dota/centermap_obb_r50_fpn_1x_dota_mask_weight.py
+++ configs/dota/centermap_obb_r50_fpn_1x_dota_mask_weight.py
@@ -123,7 +123,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', 
+        dict(type='LoadAnnotations', 
         with_bbox=True, 
         with_mask=True,
         with_mask_weight=True,
@@ -132,9 +132,8 @@ train_pipeline = [
         centermap_encode='centerness', 
         centermap_rate=0.5, 
         centermap_factor=4),
-    dict(type='Resize', img_scale=(600, 600), keep_ratio=True),
+    dict(type='Resize', img_scale=(1024, 1024), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='RandomRotate', rotate_ratio=1.0, choice=(0, 90, 180, 270)),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
@@ -156,22 +155,22 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    imgs_per_gpu=1,
-    workers_per_gpu=1,
+    imgs_per_gpu=2,
+    workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/dota_evaluation_sample_{}_{}_best_keypoint.json'.format(dataset_version, train_rate),
-        img_prefix=data_root + 'evaluation_sample/',
+        ann_file=data_root + 'annotations/dota_trainval_{}_{}_best_keypoint.json'.format(dataset_version, train_rate),
+        img_prefix=data_root + 'trainval/',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/dota_evaluation_sample_{}_{}_best_keypoint.json'.format(dataset_version, val_rate),
-        img_prefix=data_root + 'evaluation_sample/',
+        ann_file=data_root + 'annotations/dota_test_{}_{}_best_keypoint.json'.format(dataset_version, val_rate),
+        img_prefix=data_root + 'test/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
         ann_file=data_root + 'annotations/dota_test_{}_{}_best_keypoint_no_ground_truth.json'.format(dataset_version, val_rate),
-        img_prefix=data_root + 'evaluation_sample/',
+        img_prefix=data_root + 'test/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric=['bbox', 'segm'])
 # optimizer
@@ -197,7 +196,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/local_test'
+work_dir = './work_dirs/centermap_obb_r50_fpn_1x_dota_mask_weight'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
