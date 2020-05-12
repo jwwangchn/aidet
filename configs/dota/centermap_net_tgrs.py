732aa54358b7b3fd74097ba0d1da672cdb1b0da6
jwwangchn <jwwangchn@outlook.com>
2020-05-12 11:03:02 +0800

    fix the bug in config

--- configs/dota/centermap_net_tgrs.py
+++ configs/dota/centermap_net_tgrs.py
@@ -144,7 +144,8 @@ train_pipeline = [
     dict(type='LoadAnnotations', 
         with_bbox=True, 
         with_mask=True, 
-        with_seg=True, 
+        with_seg=True,
+        with_heatmap_weight=True, 
         poly2mask=False, 
         poly2centermap=True, 
         centermap_encode='centerness', 
@@ -155,6 +156,7 @@ train_pipeline = [
     dict(type='RandomRotate', rotate_ratio=1.0, choice=(0, 90, 180, 270)),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
+    dict(type='SegRescale', scale_factor=1 / 4),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks', 'gt_semantic_seg', 'gt_heatmap_weight']),
 ]
