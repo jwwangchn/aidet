846a633378d916c4d9bc31347131314aadc2ffc5
jwwangchn <jwwangchn@outlook.com>
2020-05-25 09:33:50 +0800

    sync jwwangchn codes on

--- configs/dota/dota_v009_centermap_net_r50_v1_train_rotation.py
+++ configs/dota/dota_v009_centermap_net_r50_v1_train_rotation.py
@@ -200,7 +200,7 @@ data = dict(
         classwise_nms_threshold=True))
 evaluation = dict(interval=1, metric=['bbox', 'segm'])
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
