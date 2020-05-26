99851ca8b17d6f70721473055ed96c00d85a1230
jwwangchn <jwwangchn@outlook.com>
2020-05-26 13:39:10 +0800

    sync jwwangchn codes on

--- tools/buildchange/dist_buildchange_test.sh
+++ tools/buildchange/dist_buildchange_test.sh
@@ -8,4 +8,4 @@ GPUS=$3
 PORT=${PORT:-29500}
 
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/dota_test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
+    $(dirname "$0")/buildchange_test.py $CONFIG $CHECKPOINT --launcher pytorch ${@:4}
