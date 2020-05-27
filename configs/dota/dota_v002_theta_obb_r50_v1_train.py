7df6281fdf62cedf1fd47710fa20cc64a66f4454
jwwangchn <jwwangchn@outlook.com>
2020-05-27 23:15:59 +0800

    sync jwwangchn codes on

--- configs/dota/dota_v002_theta_obb_r50_v1_train.py
+++ configs/dota/dota_v002_theta_obb_r50_v1_train.py
@@ -176,8 +176,8 @@ data = dict(
         encode='thetaobb'),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/dota_evaluation_sample_v1_best_keypoint.json',
-        img_prefix=data_root + 'evaluation_sample/',
+        ann_file=data_root + 'annotations/dota_test_{}_{}_best_keypoint.json'.format(dataset_version, val_rate),
+        img_prefix=data_root + 'test/',
         pipeline=test_pipeline,
         encode='thetaobb'))
 evaluation = dict(interval=1, metric='bbox')
