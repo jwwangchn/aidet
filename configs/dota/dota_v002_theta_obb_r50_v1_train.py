e059aba246983c6ccd52f74dcab822458ebaad60
jwwangchn <jwwangchn@outlook.com>
2020-05-27 18:21:07 +0800

    sync jwwangchn codes on

--- configs/dota/dota_v002_theta_obb_r50_v1_train.py
+++ configs/dota/dota_v002_theta_obb_r50_v1_train.py
@@ -201,7 +201,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/theta_obb_r50_fpn_1x_dota'
+work_dir = './work_dirs/dota_v002_theta_obb_r50_v1_train'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
