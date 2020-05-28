ed9535c61307af2e1be93d36ccda4ca80d550c1d
jwwangchn <jwwangchn@outlook.com>
2020-05-28 18:00:38 +0800

    sync jwwangchn codes on

--- tools/buildchange/buildchange_demo.py
+++ tools/buildchange/buildchange_demo.py
@@ -11,10 +11,11 @@ from mmdet.apis import init_detector, inference_detector, show_result
 
 def parse_args():
     parser = parser = argparse.ArgumentParser(description='DOTA Testing')
-    parser.add_argument('--dataset', default='dota', help='dataset name')
-    parser.add_argument('--dataset_version', default='v1', help='dataset name')
-    parser.add_argument('--config_version', default='mask_rcnn_r50_fpn_1x_dota', help='version of experiments (DATASET_V#NUM)')
-    parser.add_argument('--imageset', default='test', help='imageset of evaluation')
+    parser.add_argument('--dataset', default='buildchange', help='dataset name')
+    parser.add_argument('--dataset_version', default='v2', help='dataset version')
+    parser.add_argument('--city', default='shanghai', help='city name')
+    parser.add_argument('--config_version', default='bc_v002_mask_rcnn_r50_v2_jinan_roof', help='version of experiments (DATASET_V#NUM)')
+    parser.add_argument('--imageset', default='val', help='imageset of evaluation')
     parser.add_argument('--epoch', default=12, help='epoch')
     parser.add_argument('--show', action='store_true', help='show flag')
 
@@ -27,7 +28,8 @@ if __name__ == "__main__":
     # config and result files
     config_file = './configs/{}/{}.py'.format(args.dataset, args.config_version)
     checkpoint_file = './work_dirs/{}/epoch_{}.pth'.format(args.config_version, args.epoch)
-    img_dir = './data/{}/v1/{}/images'.format(args.dataset, args.imageset)
+    img_dir = './data/{}/{}/{}/images'.format(args.dataset, args.dataset_version, args.city)
+    save_dir = f'results/buildchange/{args.config_version}/{args.city}/vis/'
 
     print(config_file)
     model = init_detector(config_file, checkpoint_file, device='cuda:0')
@@ -38,6 +40,7 @@ if __name__ == "__main__":
     firstfile = True
     for img_name in img_list:
         print(img_name)
+        out_file = os.path.join(save_dir, img_name)
         img_file = os.path.join(img_dir, img_name)
         img = cv2.imread(img_file)
         if args.show:
@@ -46,5 +49,5 @@ if __name__ == "__main__":
         bboxes = np.vstack(bbox_result)
         if len(bboxes) == 0:
             continue
-        show_result(img, (bbox_result, segm_result), model.CLASSES, score_thr=0.5)
+        show_result(img, (bbox_result, segm_result), model.CLASSES, score_thr=0.5, out_file=out_file)
         # wwtool.imshow_bboxes(img, bbox_result[0][:, 0:-1], show=True)
