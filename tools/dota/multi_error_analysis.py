ca9d9cfc6754e1a254ae2205554b599afb0e320a
jwwangchn <jwwangchn@outlook.com>
2020-05-30 22:18:55 +0800

    sync jwwangchn codes on

--- tools/dota/multi_error_analysis.py
+++ tools/dota/multi_error_analysis.py
@@ -1,8 +1,8 @@
 from coco_error_analysis import analyze_results
 
 if __name__ == "__main__":
-    models = ['dota_v002_theta_obb_r50_v1_train', 'dota_v003_point_obb_r50_v1_train', 'dota_v004_h_obb_r50_v1_train']
-    titles = [r'$\theta$-based OBB', 'Point-based OBB', '$h$-based OBB']
+    models = ['dota_v016_mask_obb_r50_v1_trainval']
+    titles = [r'Mask OBB']
     for title, model in zip(titles, models):
         city = model.split('_')[-2]
 
