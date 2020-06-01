ab0a68590ec3299a66c2a2129512cdfc42b21a59
jwwangchn <jwwangchn@outlook.com>
2020-06-02 07:33:11 +0800

    sync jwwangchn codes on

--- tools/buildchange/buildchange_slurm_train.sh
+++ tools/buildchange/buildchange_slurm_train.sh
@@ -10,7 +10,9 @@ if [ $1 == 1 ]
 then
     # train but not debug
     echo "==== start no debug training, mode name = ${model} ===="
-    export CUDA_LAUNCH_BLOCKING=1
+    # export CUDA_LAUNCH_BLOCKING=1
+    set -x
+    
     srun -p ad_rs \
     -N2 \
     --job-name=bc_v009 \
