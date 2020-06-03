18096f21ec8de94002cd9e3a608f8cf7c42be84c
jwwangchn <jwwangchn@outlook.com>
2020-06-03 22:23:19 +0800

    sync jwwangchn codes on

--- configs/buildchange/bc_v009_mask_rcnn_hrnetv2p_w24_v2_roof_trainval.py
+++ configs/buildchange/bc_v009_mask_rcnn_hrnetv2p_w24_v2_roof_trainval.py
@@ -195,7 +195,7 @@ for city in cities:
     train_ann_file.append(data_root + 'annotations/buildchange_v2_train_{}.json'.format(city))
     img_prefix.append(data_root + '../' + "{}/images/".format(city))
 data = dict(
-    imgs_per_gpu=1,
+    imgs_per_gpu=2,
     workers_per_gpu=1,
     train=dict(
         type=dataset_type,
@@ -214,7 +214,7 @@ data = dict(
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric=['bbox', 'segm'])
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.04, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
