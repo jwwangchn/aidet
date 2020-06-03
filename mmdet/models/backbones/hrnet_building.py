66e64108dc4ab5e5f02815182c813dd192b8b16e
jwwangchn <jwwangchn@outlook.com>
2020-06-03 15:07:49 +0800

    sync jwwangchn codes on

--- mmdet/models/backbones/hrnet_building.py
+++ mmdet/models/backbones/hrnet_building.py
@@ -20,6 +20,13 @@ import torch.nn as nn
 import torch._utils
 import torch.nn.functional as F
 
+from mmcv.cnn import constant_init, kaiming_init
+from mmcv.runner import load_checkpoint
+from torch.nn.modules.batchnorm import _BatchNorm
+
+from mmdet.ops import build_conv_layer, build_norm_layer
+from mmdet.utils import get_root_logger
+
 from ..registry import BACKBONES
 
 import sys
