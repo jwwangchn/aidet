4057354ed0d636f8f87fa0c6ec0527c613af1f74
jwwangchn <jwwangchn@outlook.com>
2020-06-10 10:04:21 +0800

    sync jwwangchn codes on

--- configs/buildchange/bc_v014_mask_rcnn_hrnetv2p_w32_v2_roof_trainval.py
+++ configs/buildchange/bc_v014_mask_rcnn_hrnetv2p_w32_v2_roof_trainval.py
@@ -226,9 +226,9 @@ optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
     policy='step',
-    warmup='linear',
-    warmup_iters=500,
-    warmup_ratio=1.0 / 3,
+    warmup='exp',
+    warmup_iters=1000,
+    warmup_ratio=1.0 / 10,
     step=[8, 11])
 checkpoint_config = dict(interval=1)
 # yapf:disable
