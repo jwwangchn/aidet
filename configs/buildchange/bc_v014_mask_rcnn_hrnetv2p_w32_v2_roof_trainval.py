7ad265369e1b3e8eaaccd86509be5fd5232ce52b
jwwangchn <jwwangchn@outlook.com>
2020-06-10 19:23:33 +0800

    sync jwwangchn codes on

--- configs/buildchange/bc_v014_mask_rcnn_hrnetv2p_w32_v2_roof_trainval.py
+++ configs/buildchange/bc_v014_mask_rcnn_hrnetv2p_w32_v2_roof_trainval.py
@@ -221,12 +221,12 @@ data = dict(
 evaluation = dict(interval=1, metric=['bbox', 'segm'])
 # optimizer
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+optimizer_config = dict(grad_clip=dict(max_norm=10, norm_type=2))
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='exp',
-    warmup_iters=1000,
+    warmup_iters=5000,
     warmup_ratio=1.0 / 10,
     step=[8, 11])
 checkpoint_config = dict(interval=1)
